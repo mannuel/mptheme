f8d62225999a3c7bb091d200fa3948eeb9fe8d30
Manuel Padilla <manuel@plugdigital.net>
2018-01-05 17:58:04 -0600

    Adding metabox option “Show hero Revolution Slider insted of heading?” For pages

--- inc/metabox.php
+++ inc/metabox.php
@@ -4,13 +4,12 @@
  * @package mptheme
  */
 
-
 /**
  * Meta box for page setup
  */
 function page_setup_metabox(){
 	$screens = array( 'post', 'page' );
-	add_meta_box( 'mptheme-page-setup', 'Page setup', 'page_setup_metabox_content', $screens, 'side', 'low' );
+	add_meta_box( 'mptheme-page-setup', __('Page Setup', 'mptheme'), 'page_setup_metabox_content', $screens, 'side', 'low' );
 }
 
 function page_setup_metabox_content( $post ){
@@ -34,6 +33,56 @@ function page_setup_metabox_content( $post ){
 			<option value="true" <?php echo selected( $mptheme_page_setup_show_heading, 'true', false); ?>> <?php _e( 'Enable', 'mptheme' ); ?></option>
 			<option value="false" <?php echo selected( $mptheme_page_setup_show_heading, 'false', false); ?>> <?php _e( 'Disable', 'mptheme' ); ?></option>;
 		</select>
+
+	<?php
+	// if Revolution Slider is available uset can select an slider for the page
+	if(class_exists('RevSliderAdmin')) {
+		global $wpdb;
+
+		$rs = $wpdb->get_results( 
+			"
+			SELECT id, title, alias
+			FROM ".$wpdb->prefix."revslider_sliders
+			ORDER BY id ASC LIMIT 100
+			"
+		);
+		$revsliders = array(array(
+			'value' => 'no_slider',
+			'label' => 'No Slider'
+		));
+		if ($rs) {
+			$_ri = 1;
+			foreach ( $rs as $slider ) {
+				$revsliders[$_ri]['value'] = $slider->alias;
+				$revsliders[$_ri]['label'] = $slider->title;
+				$_ri++;
+			}
+		} else {
+			$revsliders["No sliders found"] = 0;
+		}
+
+		if(count($revsliders)>0 ){
+			// Get the saved slider
+			$mptheme_page_setup_revslider = get_post_meta( $post->ID, 'mptheme_page_setup_revslider', true );
+
+			if(!$mptheme_page_setup_revslider){
+				$mptheme_page_setup_revslider = 'no_slider';
+			}
+
+		?>
+
+			<p class="post-attributes-label-wrapper">
+				<label class="post-attributes-label"> <?php _e( 'Show hero Revolution Slider insted of heading?', 'mptheme' ); ?> </label>
+			</p>
+			<select name="mptheme_page_setup_revslider">
+				<?php for ($i=0; $i < count($revsliders); $i++) { ?>
+					<option value="<?php echo $revsliders[$i]["value"] ?>" <?php echo selected( $mptheme_page_setup_revslider, $revsliders[$i]["value"], false); ?>><?php echo $revsliders[$i]["label"] ?></option>
+				<?php } ?>
+			</select>
+
+		<?php }
+	}
+	?>
 
 	<?php
 }
@@ -78,6 +127,18 @@ function mptheme_layout_save_meta_box( $post_id ){
 		$sq_data = sanitize_text_field( $_POST['mptheme_page_setup_show_heading'] );
 		// Update the meta field in the database.
 		update_post_meta( $post_id, 'mptheme_page_setup_show_heading', $sq_data );
+	}
+
+	// Make sure that it is set.
+	if ( isset( $_POST['mptheme_page_setup_revslider'] ) ) {
+		// Sanitize user input.
+		$sq_data = sanitize_text_field( $_POST['mptheme_page_setup_revslider'] );
+		// Update the meta field in the database.
+		if ($sq_data == "no_slider") {
+			delete_post_meta( $post_id, 'mptheme_page_setup_revslider' );
+		} else {
+			update_post_meta( $post_id, 'mptheme_page_setup_revslider', $sq_data );
+		}
 	}
 		
 }
--- page.php
+++ page.php
@@ -5,26 +5,37 @@
  * @package mptheme
  */
 
-get_header(); ?>
+get_header();
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+// If page has a Revolution Slider set
+if ( get_post_meta($post->ID, 'mptheme_page_setup_revslider', true) ) {
 
-				get_template_part( 'template-parts/content', 'page' );
+$page_slider = get_post_meta($post->ID, 'mptheme_page_setup_revslider', true);
+	echo do_shortcode( '[rev_slider alias="'. $page_slider .'"]' );
+}
+?>
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+	<div class="container">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
 
-			endwhile; // End of the loop.
-			?>
+				<?php
+				while ( have_posts() ) : the_post();
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+					get_template_part( 'template-parts/content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+
+				endwhile; // End of the loop.
+				?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div>
 
 <?php
 get_footer();
